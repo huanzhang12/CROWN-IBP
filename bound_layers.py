1d29f8ae6fcc80ebc55b94b3d4bb21d266b2edb7
Huan Zhang <huan@huan-zhang.com>
2019-07-10 17:45:46 -0400

    cleanup and optimization

--- bound_layers.py
+++ bound_layers.py
@@ -117,16 +117,6 @@ class BoundConv2d(Conv2d):
         self.output_shape = output.size()[1:]
         return output
 
-    def reshape(self, A):
-        # TODO: avoid this permute!
-        A = A.permute(0,4,1,2,3)
-        return A.view(-1, *A.size()[2:])
-
-    def extract(self, A, batch_size):
-        # TODO: avoid this permute!
-        A = A.view(batch_size, -1, *A.size()[1:])
-        return A.permute(0,2,3,4,1)
-
     def bound_backward(self, last_A):
         logger.debug('last_A %s', last_A.size())
         shape = last_A.size()
@@ -187,94 +177,34 @@ class BoundReLU(ReLU):
         # stored upper and lower bounds will be used for backward bound propagation
         self.upper_u = h_U
         self.lower_l = h_L
-        """
-        h_U_unstab = h_U[self.unstab]
-        h_L_unstab = h_L[self.unstab]
-        tightness_loss = (-h_U_unstab * h_L_unstab / (h_U_unstab - h_L_unstab)).sum()
-        """
         tightness_loss = self.unstab.sum()
         # tightness_loss = torch.min(h_U_unstab * h_U_unstab, h_L_unstab * h_L_unstab).sum()
         return norm, F.relu(h_U), F.relu(h_L), tightness_loss, tightness_loss.detach().cpu().item(), \
                (h_U < 0).sum().detach().cpu().item(), (h_L > 0).sum().detach().cpu().item()
-        # return F.relu(h_U), F.relu(h_L), ((h_U > 0) & (h_L < 0)).sum()
-        # return F.relu(h_U), F.relu(h_L), h_U.numel()
 
     def bound_backward(self, last_A):
-        logger.debug('last_A %s', last_A.size())
-        guard_eps = 1e-5
-        # unstable neurons
-        lb = self.lower_l
-        ub = self.upper_u
-        unstab = self.unstab
-        lb_unstab = lb[unstab]
-        ub_unstab = ub[unstab]
-        """
-        upper_d = torch.zeros_like(lb)
-        lower_d = torch.zeros_like(lb)
-        logger.debug('upper_d %s', upper_d.size())
-        # active neurons
-        active = (lb > guard_eps).detach()
-        upper_d[active] = 1.0
-        lower_d[active] = 1.0
-        # inactive neurons (default)
-        # CROWN bound: upper bound
-        upper_d[unstab] = ub_unstab / (ub_unstab - lb_unstab)
-        unstab_ularge = (unstab & (ub > -lb)).detach() # a guard epsilon is added to avoid division by 0
-        # CROWN bound: choose 1.0 as the lower bound for u > |l|; otherwise 0.0
-        lower_d[unstab_ularge] = 1.0
-        # CROWN bound: for positive element in A 
-        next_A = upper_d.unsqueeze(1) * last_A.clamp(max=0) + lower_d.unsqueeze(1) * last_A.clamp(min=0)
-        """
-        ub_slope = ub_unstab / (ub_unstab - lb_unstab)
-        # copy and fill inactive elements with 0, other elements are multiplied by 1 automatically
-        # for active neurons, we don't need to do anything. Inactive neurons are filled with 0
-        inactive = (ub < -guard_eps).detach()
-        next_A = last_A.masked_fill(inactive.unsqueeze(1), 0.0)
-        # indices of all unstable neurons
-        unstab_s = unstab.view(self.unstab.size(0), -1).nonzero()
-        # now fill in values for all unstable neurons.
-        # we first transpose last_A to (output_shape, batch, *layer_size)
-        # those are unstable elements
-        unstable_A = torch.transpose(last_A,0,1).masked_select(unstab.unsqueeze(0))
-        # now, we get the A's element for all unstable neurons, shape (output_shape, all unstable neurons in batches)
-        unstable_A = unstable_A.view(last_A.size(1), -1)
-        # for element in A < 0 we choose u/(u-l)
-        A_neg_slopes = unstable_A.clamp(max=0) * ub_slope
-        # for element in A > 0, we choose the flexible lower bound
-        # for the unstable neurons with |LB| < |UB| we will choose 1 as the lower slope
-        lb_slope = torch.zeros_like(ub_slope)
-        lb_slope[ub_unstab > -lb_unstab] = 1.0
-        A_pos_slopes = unstable_A.clamp(min=0) * lb_slope
-        # now form the slopes for all unstable neurons
-        unstable_slopes = A_pos_slopes + A_neg_slopes
-        # and fill them into the unstable neuron places, similiar to the way we get them
-        next_A = torch.transpose(next_A,0,1).masked_scatter_(unstab.unsqueeze(0), unstable_slopes)
-        # swap back to the original order
-        next_A = torch.transpose(next_A,0,1)
-
-        logger.debug('next_A %s', next_A.size())
-        # choose the bias term according to next_A
-        mult_A = next_A.view(last_A.size(0), last_A.size(1), -1)
-        logger.debug('next_A reshape %s', mult_A.size())
-        llb = torch.zeros_like(lb)
-        llb[unstab] = lb_unstab # llb is actually the intercept of UPPER bound
-        sum_bias = mult_A.clamp(max=0).matmul(-llb.view(llb.size(0), -1, 1)).squeeze(-1)
-        # print('relu', sum_bias)
-        logger.debug('sum_bias %s', sum_bias.size())
-        # done, delete saved bounds
+        lb_r = self.lower_l.clamp(max=0)
+        ub_r = self.upper_u.clamp(min=0)
+        # avoid division by 0 when both lb_r and ub_r are 0
+        ub_r = torch.max(ub_r, lb_r + 1e-8)
+        # ub_r = F.softplus(self.upper_u, beta=50)
+        # lb_r = - F.softplus(self.lower_l, beta=50)
+        # CROWN upper and lower linear bounds
+        upper_d = ub_r / (ub_r - lb_r)
+        upper_b = - lb_r * upper_d
+        upper_d = upper_d.unsqueeze(1)
+        lower_d = (upper_d > 0.5).float()
+        # lower_d = torch.sigmoid(100 * upper_d - 50) # differentiable version of lower slope (any slope between 0 and 1 is a valid bound)
+        # Choose upper or lower bounds based on the sign of last_A
+        neg_A = last_A.clamp(max=0)
+        pos_A = last_A.clamp(min=0)
+        next_A = upper_d * neg_A + lower_d * pos_A
+        mult_A = neg_A.view(last_A.size(0), last_A.size(1), -1)
+        sum_bias = mult_A.matmul(upper_b.view(upper_b.size(0), -1, 1)).squeeze(-1)
         del self.upper_u
         del self.lower_l
-        del self.unstab
         return next_A, sum_bias
 
-    def forward(self, input):
-        output = super(BoundReLU, self).forward(input)
-        # give an initial bound such that even without bound propagation, a global Lipschitz constant can be obtained
-        # self.upper_u = torch.ones_like(input).unsqueeze(-1)
-        # self.upper_u *= np.inf
-        # self.lower_l = torch.ones_like(input).unsqueeze(-1)
-        # self.lower_l *= -np.inf
-        return output
 
 class BoundSequential(Sequential):
     def __init__(self, *args):
