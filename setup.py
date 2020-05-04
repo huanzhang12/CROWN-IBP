02b0e6cc7690fb5b144ae9dad888e43d3d99c6c0
Michael Everett <mfe@mit.edu>
2020-05-04 17:06:11 -0400

    tmp

--- setup.py
+++ setup.py
@@ -3,18 +3,7 @@ from setuptools import setup, find_packages
 setup(name='crown_ibp',
       version='1.0.0',
       install_requires=[
-          'numpy',
           'torch',
           ],
       packages=find_packages()
 )
-'''
-packages=['', 'verification_crown', 'verification_crown_vector_epsilon'],
-package_dir={
-  '': 'verification_crown', 
-  'verification_crown': 'verification_crown',
-  'verification_crown_vector_epsilon': 'verification_crown_vector_epsilon'
-  },
-'''
-
-
